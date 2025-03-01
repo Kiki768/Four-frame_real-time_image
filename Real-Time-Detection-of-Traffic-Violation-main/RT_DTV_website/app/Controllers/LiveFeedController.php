fb8ac05e5cbbd5617b11090e0718560843a198b6
weikikiii <132288830+weikikiii@users.noreply.github.com>
2025-03-01 23:42:45 +0800

    Update LiveFeedController.php
    新增從資料夾載入影片

--- Real-Time-Detection-of-Traffic-Violation-main/RT_DTV_website/app/Controllers/LiveFeedController.php
+++ Real-Time-Detection-of-Traffic-Violation-main/RT_DTV_website/app/Controllers/LiveFeedController.php
@@ -6,7 +6,15 @@ class LiveFeedController extends BaseController
 {
     public function index()
     {
-        return view('live/index'); // 讓它載入 live/index.php
+        $videoPath = base_url('videos'); // 指向public/videos
+        $files = glob(FCPATH . 'videos/*.mp4');
+        $videos = [];
+
+        foreach($files as $file)
+        {
+            $videos[] = $videoPath . '/' . basename($file);
+        }
+        return view('live/index', ['videos' => $videos]); // 讓它載入 live/index.php
     }
 }
 
