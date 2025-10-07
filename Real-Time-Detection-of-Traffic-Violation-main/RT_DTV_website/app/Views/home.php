f15f589bb4bcb7aa1f20914fcbc7bebc1961639a
weikikiii <vickylin930329@gmail.com>
2025-10-08 00:22:50 +0800

    更新主頁

--- Real-Time-Detection-of-Traffic-Violation-main/RT_DTV_website/app/Views/home.php
+++ Real-Time-Detection-of-Traffic-Violation-main/RT_DTV_website/app/Views/home.php
@@ -1,10 +1,24 @@
 <?= $this->extend('template') ?>
 <?= $this->section('content') ?>
     <div class="img-container">
-        <img src="pictures/system.png" alt="system">
         <img src="pictures/tutorial_1.png" alt="tutorial">
         <img src="pictures/tutorial_2.png" alt="tutorial">
         <img src="pictures/tutorial_3.png" alt="tutorial">
     </div> 
+
+    <style>
+    .img-container {
+        text-align: center; /* 圖片整體置中 */
+    }
+    .img-container img {
+        width: 80%;
+        max-width: 3000px; /* 最大寬度 300px */
+        height: auto;
+        margin: 10px;
+        border-radius: 8px;
+    }
+    </style>
+
+
 <?= $this->endSection() ?>
 
